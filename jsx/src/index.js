3cf7424321009beaa013fae84a91a1e57b7080ea
semihlevent <semihlt@hotmail.com>
2020-09-08 00:30:41 +0300

    first two projects of repository

---
+++ jsx/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+const App = () => {
+    const buttonText = 'Click me baby';
+
+    return (
+        <div>
+            <label className="label" htmlFor="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText}
+            </button>
+        </div>
+    );
+};
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
