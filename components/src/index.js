5d677c2f80bb5174ff71ac0d8397d7d9ae05aa1e
semihlevent <semihlt@hotmail.com>
2020-09-10 00:36:18 +0300

    passing props from parent to child

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,18 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail author="Sem" 
+                timeAgo ="Today at 4:45PM" 
+                content="Nice post body!" 
+                avatar={faker.image.avatar()} />
+            <CommentDetail author="Alex" 
+                timeAgo ="Today at 2:00PM" 
+                content="You are the best dude!"
+                avatar={faker.image.avatar()} />
+            <CommentDetail author="Jane" 
+                timeAgo ="Yesterday at 5:00PM" 
+                content="Oh my god dear!"
+                avatar={faker.image.avatar()} />
         </div>
     );
 };
