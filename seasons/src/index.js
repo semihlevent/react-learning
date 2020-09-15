3539bb9775f48fcfdc163b726c09f85ba9fd6806
semihlevent <semihlt@hotmail.com>
2020-09-16 00:51:55 +0300

    life cycle trials

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,27 +6,32 @@ import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
+    state = {lat: null, errorMessage: ''};
+
+
     constructor(props) {
         super(props);
 
         //this is the only time we do direct assignment
-        this.state = {lat: null, errorMessage: ''};
+        
+        
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message });
-            }
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         );
     }
 
+    componentDidUpdate() {
+        console.log("My componenet was updated - it rerendered.");
+    }
 
     render() {
         
         if (this.state.lat && !this.state.errorMessage) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         } else if (!this.state.lat && this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
