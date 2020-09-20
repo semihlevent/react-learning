e55c8f82da1d4d21ad4bb1cbd2118aeb54d886e3
semihlevent <semihlt@hotmail.com>
2020-09-20 15:56:36 +0300

    added styling and refactored index.js render method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 
@@ -28,14 +29,22 @@ class App extends React.Component {
         console.log("My componenet was updated - it rerendered.");
     }
 
-    render() {
-        
+    renderContent() {
         if (this.state.lat && !this.state.errorMessage) {
             return <SeasonDisplay lat={this.state.lat} />;
         } else if (!this.state.lat && this.state.errorMessage) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        return <div>Loading...</div>;
+        return <Spinner message="Please accept location request..." />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+        
     }
 
 }
